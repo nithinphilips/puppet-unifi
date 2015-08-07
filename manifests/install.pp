0cdb2e6f746afd195e0398969c19d64396b2ae77
David Logie <djl@mksh.org>
2015-08-07 10:16:00 +0100

    The unifi repos changed again.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,8 @@
 # == Class: unifi::install
 class unifi::install inherits unifi::params {
   apt::source { 'ubiquiti':
-    location => 'http://www.ubnt.com/downloads/unifi/distros/deb/debian',
-    release  => 'debian',
+    location => 'http://www.ubnt.com/downloads/unifi/debian',
+    release  => 'stable',
     repos    => 'ubiquiti',
     key      => {
       'id'        => '4A228B2D358A5094178285BE06E85760C0A52C50',
