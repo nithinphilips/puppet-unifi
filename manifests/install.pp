fe503ab1b5672d763cabc445583da6b4ae120245
Nithin Philips <nithin@nithinphilips.com>
2017-01-17 14:19:26 -0500

    Update install.pp
    Use unifi5 repo.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@
 class unifi::install inherits unifi::params {
   apt::source { 'ubiquiti':
     location => 'http://www.ubnt.com/downloads/unifi/debian',
-    release  => 'stable',
+    release  => 'unifi5',
     repos    => 'ubiquiti',
     key      => {
       'id'        => '4A228B2D358A5094178285BE06E85760C0A52C50',
